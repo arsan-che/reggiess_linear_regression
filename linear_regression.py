9615caa75c70cad9f0f63a1ea85921c30897ba01
arsan-che <arsan903@gmail.com>
2025-02-17 13:31:46 +0000

    calcuated error and distance between the points and tested the code with different functions

--- linear_regression.py
+++ linear_regression.py
@@ -5,3 +5,16 @@ def get_y(m, b, x):
 
 get_y(1, 0, 7) == 7
 get_y(5, 10, 3) == 25
+
+#calculating error and distance between the points
+def calculate_error(m, b, point):
+  x_point, y_point = point
+  y = m*x_point + b
+  distance = abs(y - y_point)
+  return distance
+
+#testing the function
+print(calculate_error(1, 0, (3, 3)))
+print(calculate_error(1, 0, (3, 4)))
+print(calculate_error(1, -1, (3, 3)))
+print(calculate_error(-1, 1, (3, 3)))
